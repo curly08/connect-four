0b4cf3f0fae8773309f9ee087ee992cd1dec7be9
curly08 <mattynoakes08@gmail.com>
2021-10-25 15:28:02 -0400

    Add tests for ConnectFour#establish_players

--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -3,3 +3,23 @@
 # rubocop:disable all
 
 require_relative '../lib/connect_four'
+require_relative '../lib/player'
+
+describe ConnectFour do
+  subject(:game) { described_class.new }
+
+  # establish players
+  describe '#establish_players' do
+    context 'two players are created' do
+      before do
+        allow(game).to receive(:puts)
+        allow(game).to receive(:gets).and_return('matt', 'gary')
+      end
+      
+      it 'creates two Player objects' do
+        expect(Player).to receive(:new).twice
+        game.establish_players
+      end
+    end
+  end
+end
