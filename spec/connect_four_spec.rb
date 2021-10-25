a0041a76f6742567fff58e0c2e8b09815a6af792
curly08 <mattynoakes08@gmail.com>
2021-10-25 15:56:47 -0400

    Add first test for ConnectFour#play_move

--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -15,10 +15,30 @@ describe ConnectFour do
         allow(game).to receive(:puts)
         allow(game).to receive(:gets).and_return('matt', 'gary')
       end
-      
+
       it 'creates two Player objects' do
         expect(Player).to receive(:new).twice
         game.establish_players
+      end
+    end
+  end
+
+  # player plays move
+  describe '#play_move' do
+    let(:player) { instance_double(Player, 'Matt') }
+
+    context 'when player picks valid column(1..7) to place mark' do
+      before do
+        allow(game).to receive(:puts)
+        allow(player).to receive(:name)
+      end
+
+      it 'receives input and verifies its validity' do
+        input = '1'
+        expect(game).to receive(:input_valid?).and_return(false, true)
+        expect(game).to receive(:gets).and_return(input)
+        expect(game).to receive(:place_mark)
+        game.play_move(player)
       end
     end
   end
